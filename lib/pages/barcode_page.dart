21e235835d65bc1c38d34d6bf8778435eed822bb
HichamGsb <hichamgsb.pro@gmail.com>
2023-12-14 10:52:47 +0100

    Implementation of the ScannerPage (camera)

--- lib/pages/barcode_page.dart
+++ lib/pages/barcode_page.dart
@@ -1,3 +1,4 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 class BarCodePage extends StatefulWidget {
@@ -8,8 +9,52 @@ class BarCodePage extends StatefulWidget {
 }
 
 class _BarCodePageState extends State<BarCodePage> {
+  late List<CameraDescription> cameras;
+  late CameraController cameraController;
+
+  @override
+  void initState() {
+    startCamera();
+    super.initState();
+  }
+
+  void startCamera() async {
+  cameras = await availableCameras();
+
+  cameraController = CameraController(
+      cameras[0],
+      ResolutionPreset.max,
+      enableAudio: false
+  );
+
+  await cameraController.initialize().then((value) {
+    if (!mounted) {
+      return;
+    }
+    setState(() {});
+  }).catchError((e) {
+    print(e);
+  });
+  }
+
+  @override
+  void dispose() {
+    cameraController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    if(cameraController.value.isInitialized) {
+      return Scaffold(
+        body: Stack(
+          children: [
+            CameraPreview(cameraController),
+          ],
+        )
+      );
+    } else {
+      return const SizedBox();
+    }
   }
 }
