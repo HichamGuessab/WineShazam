85a8da266095b2c3e059e6bdbda9fc1da204edbb
HichamGsb <hichamgsb.pro@gmail.com>
2023-12-14 12:36:10 +0100

    Implementation of the two directions of the Camera

--- lib/pages/barcode_page.dart
+++ lib/pages/barcode_page.dart
@@ -12,18 +12,20 @@ class _BarCodePageState extends State<BarCodePage> {
   late List<CameraDescription> cameras;
   late CameraController cameraController;
 
+  int direction = 0;
+
   @override
   void initState() {
-    startCamera();
+    startCamera(0);
     super.initState();
   }
 
-  void startCamera() async {
+  void startCamera(int direction) async {
   cameras = await availableCameras();
 
   cameraController = CameraController(
-      cameras[0],
-      ResolutionPreset.max,
+      cameras[direction],
+      ResolutionPreset.high,
       enableAudio: false
   );
 
@@ -46,10 +48,29 @@ class _BarCodePageState extends State<BarCodePage> {
   @override
   Widget build(BuildContext context) {
     if(cameraController.value.isInitialized) {
-      return Scaffold(
-        body: Stack(
+      return Center(
+        child: Stack(
           children: [
             CameraPreview(cameraController),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  direction = direction == 0 ? 1 : 0;
+                  startCamera(direction);
+                });
+              },
+              child : button(Icons.flip_camera_ios_outlined, Alignment.bottomLeft),
+            ),
+            GestureDetector(
+              onTap: () {
+                cameraController.takePicture().then((value) => {
+                  if(mounted) {
+                    // Envoie vers le vin en question
+                  }
+                });
+              },
+              child : button(Icons.camera_alt_outlined, Alignment.bottomCenter),
+            ),
           ],
         )
       );
@@ -57,4 +78,35 @@ class _BarCodePageState extends State<BarCodePage> {
       return const SizedBox();
     }
   }
+}
+
+Widget button(IconData icon, Alignment alignment) {
+  return Align(
+    alignment: alignment,
+    child: Container(
+        margin: const EdgeInsets.only(
+            left: 20,
+            bottom: 20
+        ),
+        height: 50,
+        width: 50,
+        decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26,
+                  offset: Offset(2, 2),
+                  blurRadius: 10
+              )
+            ]
+        ),
+        child: Center(
+            child: Icon(
+              icon,
+              color: Colors.black87,
+            )
+        )
+    ),
+  );
 }
